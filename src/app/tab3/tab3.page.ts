496ffdff5a3200e8fb77619f52740eb0b2561138
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-01 08:39:13 +0200

    Added Add Booking

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -9,10 +9,52 @@ import { AppointmentService } from '../services/appointment.service';
   styleUrls: ['./tab3.page.scss'],
 })
 export class Tab3Page implements OnInit {
+  apform!: FormGroup;
+  isSubmitted = false;
+  id: any;
 
-  constructor() { }
+  get errorControl(){
+    return this.apform.controls;
+  }
+
+  constructor(public formBuilder: FormBuilder,
+    public appointmentService: AppointmentService,
+    private actRoute: ActivatedRoute,
+    public router: Router,) {
+      this.id = this.actRoute.snapshot.paramMap.get('id');
+      this.appointmentService.getBooking(this.id).subscribe((data: any) => {
+        this.apform = this.formBuilder.group({
+          name: [data ['name']],
+          surname: [data ['surname']],
+          email: [data['email']],
+          phone: [data['phone']],
+          date: [data['date']],
+        })
+      });
+    }
 
   ngOnInit() {
+    this.apform = this.formBuilder.group({
+      name : ['', Validators.required],
+      surname : ['', Validators.required],
+      date : ['', Validators.required],
+      email : ['',[ Validators.required, Validators.email]],
+      phone : ['', [Validators.required, Validators.minLength(8)]]
+    })
+    // this.apform = this.formBuilder.group({
+    //   name: [''],
+    //   email: [''],
+    //   mobile: ['']
+    // })
+    // console.log(this.updateBookingForm.value)
+  }
+  updateForm() {
+    // this.appointmentService.updateBooking(this.id, this.apform.value)
+    //   .then((res) => {
+    //     this.router.navigate(['/appointment-list']);
+    //   })
+    //   .catch((error: any) => console.log(error));
+    this.appointmentService.updateBooking(this.id, this.apform.value);
   }
 
 }
