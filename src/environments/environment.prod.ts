47812a66749a58aab748ab6d839bfe8b4153edde
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-01 08:17:08 +0200

    Added Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyClVXIWkAT7RMUpjMXVr10y3s_vYoPiUYU",
+    authDomain: "fir-angular-f5981.firebaseapp.com",
+    databaseURL: "https://fir-angular-f5981-default-rtdb.firebaseio.com",
+    projectId: "fir-angular-f5981",
+    storageBucket: "fir-angular-f5981.appspot.com",
+    messagingSenderId: "887022544821",
+    appId: "1:887022544821:web:db56c8acd055467641dbfc",
+    measurementId: "G-L8PQ3Z6VMF"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyClVXIWkAT7RMUpjMXVr10y3s_vYoPiUYU",
+    authDomain: "fir-angular-f5981.firebaseapp.com",
+    databaseURL: "https://fir-angular-f5981-default-rtdb.firebaseio.com",
+    projectId: "fir-angular-f5981",
+    storageBucket: "fir-angular-f5981.appspot.com",
+    messagingSenderId: "887022544821",
+    appId: "1:887022544821:web:db56c8acd055467641dbfc",
+    measurementId: "G-L8PQ3Z6VMF"
+  },
 };
 
 /*
