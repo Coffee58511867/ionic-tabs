6b396e135abea93516aafe1dcca60ce4684cb3f9
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-01 08:28:36 +0200

    Added Registerr

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -10,9 +10,38 @@ import { AppointmentService } from '../services/appointment.service';
 })
 export class Tab2Page implements OnInit {
 
-  constructor() { }
+  apform!: FormGroup;
+  isSubmitted = false;
+  constructor(public formBuilder: FormBuilder,
+    public appointmentService: AppointmentService,
+    public router: Router,
+    ) { }
 
   ngOnInit() {
+    this.apform = this.formBuilder.group({
+      name : ['', Validators.required],
+      surname : ['', Validators.required],
+      date : ['', Validators.required],
+      email : ['',[ Validators.required, Validators.email]],
+      phone : ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  submitForm(){
+    this.isSubmitted= true;
+    if(this.apform.valid){
+      console.log(this.apform.value)
+      this.appointmentService.book(this.apform.value).then((res: any) => {
+        console.log(res)
+        this.apform.reset();
+        this.router.navigate(['/tabs']);
+      })
+        .catch((error: any) => console.log(error));
+    }
+
+  }
+  get errorControl(){
+    return this.apform.controls;
   }
 
 }
