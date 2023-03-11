d86ff55e7d91dcf4d340936dc54f74640cbbbf27
Coffee58511867 <marobahlalele910@gmail.com>
2023-03-11 17:31:15 +0200

    Modified Dashboard

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -36,21 +36,17 @@ export class HomePage {
       console.log(this.loginform.value)
       this.authService
       .SignIn(email.value, password.value)
-      if(this.authService.isEmailVerified){
-        this.router.navigate(['tabs']);
-      }
-
-      // .then((res) => {
-      //   if (this.authService.isEmailVerified) {
-      //     this.router.navigate(['tabs']);
-      //   } else {
-      //     window.alert('Email is not verified');
-      //     return false;
-      //   }
-      // })
-      // .catch((error) => {
-      //   window.alert(error.message);
-      // });
+      .then((res) => {
+        if (this.authService.isEmailVerified) {
+          this.router.navigate(['tabs']);
+        } else {
+          window.alert('Email is not verified');
+          return false;
+        }
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
     }
 
   }
--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { UserService } from '../api/user.service';
 
 @Component({
   selector: 'app-tab4',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
-  constructor() { }
+  public admin = false;
+
+  constructor(
+    public authService: AngularFireAuth,
+    public firestore: AngularFirestore,
+    public userService: UserService,
+  ) { }
 
   ngOnInit() {
+    this.authService.onAuthStateChanged(user => {
+      if(user){
+       this.firestore.doc(`/users/${user.uid}`)
+       .get()
+
+      }
+    })
   }
 
 }
